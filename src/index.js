a7672af21c4e117eba9d93032d8e936925283c57
Hritik Gautam <hritikgautam19@gmai.com>
2026-05-05 00:16:46 +0530

    Connecting the db only on index.js -- index is polluted

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from 'mongoose'
+import {DB_NAME} from './constants'
+
+import express from 'express'
+const app = express();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+        app.on("error", (error)=>{
+            console.log("Application not able to talk to db");
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on Port : ${process.env.PORT}`);
+        })
+
+    } catch(error){
+        console.error("Error: ", error)
+        throw error
+    }
+} )()
